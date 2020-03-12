fb69d54635857a7ae86127d9b184aeb2a9cb730c
kj-ninja <jakubiak.krzy@gmail.com>
2020-03-12 20:28:36 +0100

    fix some layout bugs

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,13 +8,15 @@ const About = () => {
     return (
         <Container className="about__container">
             <Row>
-                <Col xs={12} lg={12} xl={6} className="about__info">
-                    <h2>O nas</h2>
-                    <img src={require("../../assets/Decoration.svg")} alt=""/>
-                    <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya
-                        nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
-                    <div className="about__info--signature">
-                        <img src={require("../../assets/Signature.svg")} alt=""/>
+                <Col xs={12} lg={12} xl={6} className="about__info d-flex align-items-center justify-content-center">
+                    <div className="d-flex flex-column align-items-center justify-content-center">
+                        <h2>O nas</h2>
+                        <img src={require("../../assets/Decoration.svg")} alt=""/>
+                        <p>Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya
+                            nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                        <div className="about__info--signature">
+                            <img src={require("../../assets/Signature.svg")} alt=""/>
+                        </div>
                     </div>
                 </Col>
                 <Col xs={12} lg={12} xl={6} className="about__image"/>
